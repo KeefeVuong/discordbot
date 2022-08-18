ea88ebaf79d9caddb0d3625f82d81e4f9b802086
Keefe Vuong <keefevuong123@gmail.com>
2022-08-18 18:52:24 +1000

    Started

--- discord_bot.py
+++ discord_bot.py
@@ -1,76 +1,148 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+import pafy
 import os
+import urllib.request
+import re
 
-client = commands.Bot(command_prefix="!")
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
 song_id_count = 0
-
-queue = {}
 channel = None
-def add_to_queue(guild, song):
-    if guild.id not in queue:
-        queue[guild.id] = []
-    queue[guild.id].append(song)
+global queues
+queues = {}
+repeat_status = False
 
 @client.event
 async def on_ready():
     print("I am online as {0.user}!".format(client))
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-    
-#     if message.content.startswith("!hello"):
-#         await message.author.voice.channel.connect()
-#         await message.channel.send("Hi there!")
+def check_queue(ctx, id):
+    global repeat_status
+    global song_id_count
+    if len(queues) > 0 and queues[id] != []:
+        voice = ctx.guild.voice_client
+        if (repeat_status): 
+            source = queues[id][song_id_count]
+            song_id_count += 1
+            if (song_id_count == len(queues)):
+                print("hello")
+                song_id_count = 0
+        else:
+            source = queues[id].pop(0)
+        voice.play(source["url"], after=lambda x=0: check_queue(ctx, ctx.message.guild.id))
+
+@client.command()
+async def repeat(ctx):
+    global repeat_status
+    if repeat_status is False:
+        await ctx.send("Repeat mode is turned on.")
+        repeat_status = True
+    else:
+        await ctx.send("Repeat mode is turned off.")
+        repeat_status = False
+
+@client.command()
+async def remove(ctx, n):
+    if len(queues) <= 0 or queues[ctx.message.guild.id] == []:
+        await ctx.send("There are no songs in the queue.")
+        return
+    else:
+        for i, item in enumerate(queues[ctx.message.guild.id]):
+            if i == int(n):
+                name = item["title"]
+                await ctx.send(f"{name} has been removed from the queue.")
+                queues[ctx.message.guild.id].remove(item)
+                return
+
+        await ctx.send(f"Invalid number. Please enter a number between 0 and {i}")
+        return
+
+@client.command()
+async def queue(ctx):
+    if len(queues) <= 0 or queues[ctx.message.guild.id] == []:
+        await ctx.send("There are no songs in the queue.")
+        return
+    else:
+        string = "```"
+        for i, item in enumerate(queues[ctx.message.guild.id]):
+            string += str(i) + ": " + item["title"] + "\n"
+
+        string += "```"
+        await ctx.send(string)
+        return
+
+
+@client.command()
+async def clear(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    queues.clear()
+    voice.stop()
+    await ctx.send("Music is stopped and the queue is cleared.")
 
 @client.command()
 async def play(ctx, url):
     global song_id_count
     global channel
-    song_path = os.path.isfile("song.webm")
-    add_to_queue(ctx.message.guild, url)
-
-    # try:
-    #     if song_path:
-    #         #os.remove("./song.webm")
-    #         song = True
-        
-    # except PermissionError:
-    #     await ctx.send("Wait for the current song to end or use '!stop' to stop the current song")
-    #     return
     
+    if ("https://" not in url or "http://" not in url):
+        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + url)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        url = 'https://www.youtube.com/watch?v=' + video_ids[0]
+
+
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
     if not voice:
-    #     await ctx.send("Already connected to a voice channel.")
-    #     song_urls.append(url)
         if ctx.message.author.voice:
             channel = ctx.message.author.voice.channel
             voice = await channel.connect()
         else:
             await ctx.send("You are not connected to any voice channels.")
             return
-    
-    ydl_opts = {
-        "format": "249/250/251",
+
+    song = pafy.new(url)
+
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+
+    audio = song.getbestaudio()
+
+    source = {
+        "url": discord.FFmpegOpusAudio(audio.url, **FFMPEG_OPTIONS),
+        "title": song.title
     }
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        song = queue[channel.guild.id][0]
-        if song == None:
-            return
-        queue[channel.guild.id].remove(song)
-        ydl.download([song])
-        song_id_count += 1
+    guild_id = ctx.message.guild.id
+    if guild_id in queues:
+        queues[guild_id].append(source)
+    else:
+        queues[guild_id] = [source]
 
-    for file in os.listdir("./"):
-        if file.endswith(".webm"):
-            os.rename(file, f"song{song_id_count}.webm")
-    
-    await voice.play(discord.FFmpegOpusAudio(f"song{song_id_count}.webm"))
+    await ctx.send(f'{song.title} was added to the queue')
+
+    # ctx.voice_client.stop()
+    # voice.play(discord.FFmpegOpusAudio(audio.url, **FFMPEG_OPTIONS))
+    if (not voice.is_playing()):
+        check_queue(ctx, guild_id)
+        voice.is_playing()
+        await ctx.send('Playing: '+ song.title)
+
+@client.command()
+async def skip(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if len(queues) <= 0 or queues[ctx.message.guild.id] == []:
+        if voice.is_playing():
+            ctx.voice_client.stop()
+            voice.is_playing()
+            await ctx.send("Skipped.")
+        else:
+            await ctx.send("There are no songs in the queue.")
+            return
+    else:
+        voice.stop()
+        check_queue(ctx, ctx.message.guild.id)
+        await ctx.send('Skipped.')
 
 @client.command()
 async def leave(ctx):
